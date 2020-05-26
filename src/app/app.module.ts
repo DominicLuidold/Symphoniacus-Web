213f60ba1cfb7ca51365166610be6b482dee52ff
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-05-26 14:26:01 +0200

    Add home/landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,22 @@ import { MaterialModule } from './material-module';
 
 // Custom components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavBarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
