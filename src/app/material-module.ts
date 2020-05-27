f753f008cb9198a409e636263a4160fa5602069a
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-05-27 21:08:32 +0200

    Implement login functionality

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatInputModule,
+    MatSnackBarModule,
     MatToolbarModule,
   ]
 })
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '../_services/authentication.service';
+import { User } from '../_models/user';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+    this.authenticationService.user.subscribe(x => this.user = x);
+  }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 
 }
