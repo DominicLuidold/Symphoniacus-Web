ba72bbe4bf433be138345c51a583de840c344e76
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-05-30 00:15:16 +0200

    Add unit test for custom pipe

--- src/app/_pipes/musical-piece-divider.pipe.ts
+++ src/app/_pipes/musical-piece-divider.pipe.ts
@@ -6,8 +6,15 @@ import { MusicalPiece } from '@app/_models';
 })
 export class MusicalPieceDividerPipe implements PipeTransform {
 
+  /**
+   * Transforms an array of {@link MusicalPiece}s into a table friendly string.
+   *
+   * @param musicalPieces An array of Musical Pieces
+   */
   transform(musicalPieces: MusicalPiece[]): string {
-    if (musicalPieces.length < 2) {
+    if (musicalPieces.length === 0) {
+      return '';
+    } else if (musicalPieces.length === 1) {
       return musicalPieces[0].name;
     } else {
       let result = '';
