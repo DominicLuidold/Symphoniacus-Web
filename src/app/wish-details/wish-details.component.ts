632155431084546ac8445722e0b40fcb945472b2
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-06-03 11:05:49 +0200

    Add possibility to edit Date Wishes in Wish Details view

--- src/app/wish-details/wish-details.component.ts
+++ src/app/wish-details/wish-details.component.ts
@@ -7,6 +7,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { BaseWish, Duty } from '@app/_models';
 import { WishService } from '@app/_services';
 import { DeleteWishDialogComponent } from '@app/delete-wish-dialog/delete-wish-dialog.component';
+import { EditDateWishDialogComponent } from '@app/edit-date-wish-dialog/edit-date-wish-dialog.component';
 import { EditDutyWishDialogComponent } from '@app/edit-duty-wish-dialog/edit-duty-wish-dialog.component';
 import { forkJoin, Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -67,10 +68,27 @@ export class WishDetailsComponent implements OnInit, OnDestroy {
 
   editWish(wish: BaseWish) {
     if (wish.target === 'DATE') {
-      // TODO
+      this.editDateWishDialog(wish);
     } else {
       this.editDutyWishDialog(wish);
     }
+  }
+
+  editDateWishDialog(dateWish: BaseWish): void {
+    const dialogRef = this.dialog.open(EditDateWishDialogComponent, {
+      width: '600px',
+      data: {
+        dateWish
+      }
+    });
+    dialogRef.componentInstance.wishUpdate.subscribe(error => {
+      if (error) {
+        this.openSnackBar(error);
+      } else {
+        this.openSnackBar('Successfully edited Request');
+        this.loadWishes();
+      }
+    });
   }
 
   editDutyWishDialog(dutyWish: BaseWish): void {
